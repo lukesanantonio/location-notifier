8c0189d99881aeeaf44b25c2c3d94673f2fc4827
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-09-26 17:51:28 -0400

    Support pin radius adjust and an animated camera

--- mobile/src/main/java/net/redcrane/location_notify/AddLocationActivity.kt
+++ mobile/src/main/java/net/redcrane/location_notify/AddLocationActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.SeekBar
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 
@@ -11,14 +12,51 @@ import kotlinx.android.synthetic.main.activity_add_location.*
 import kotlinx.android.synthetic.main.content_add_location.*
 
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.Circle
-import com.google.android.gms.maps.model.CircleOptions
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+
+const val METERS_PER_MILE = 1609.344;
+
+const val OUTER_ZOOM_LEVEL = 15.0f;
+const val INNER_ZOOM_LEVEL = 5.0f;
 
 class AddLocationActivity : AppCompatActivity(), OnMapReadyCallback {
     private var marker: Marker? = null
     private var circle: Circle? = null
+    private var googleMap: GoogleMap? = null
+
+    fun updateMilesFromSeekBar() {
+        updateMilesFromSeekBar(radiusSeekBar.progress.toDouble())
+    }
+    fun updateMilesFromSeekBar(meters: Double) {
+        // Update the circle around the pin if necessary.
+        circle?.let { it.radius = meters }
+
+        // Update the text view with the miles
+        val miles = meters / METERS_PER_MILE
+        miles_view.text = resources.getString(R.string.miles_indicator, miles);
+    }
+
+    fun zoomToMarker() {
+        // Find out the radius of the circle.
+        googleMap?.let {
+            // In meters
+            val radius = radiusSeekBar.progress.toDouble()
+
+            val lat = radius / 111111.0
+            val long = (radius / 111111.0) * Math.cos(Math.toRadians(lat))
+
+            val map = it
+            marker?.let {
+                val place = it.position;
+                val northeast = LatLng(place.latitude - lat, place.longitude - long * 1.5)
+                val southwest = LatLng(place.latitude + lat, place.longitude + long * 1.5)
+                val cam = CameraUpdateFactory.newLatLngBounds(LatLngBounds(northeast, southwest), 0)
+                map.animateCamera(cam)
+            }
+        }
+        // Use the radius to build a LatLngBounds object.
+        // Zoom to that bounds object.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +65,13 @@ class AddLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
         (map as SupportMapFragment).getMapAsync(this)
 
+        updateMilesFromSeekBar()
+
         radiusSeekBar.setOnSeekBarChangeListener(
                 object : SeekBar.OnSeekBarChangeListener {
                     override fun onProgressChanged(self: SeekBar?, newRadius: Int, fromUser: Boolean) {
-                        circle?.let { it.radius = newRadius.toDouble() }
+                        updateMilesFromSeekBar(newRadius.toDouble())
+                        zoomToMarker()
                     }
 
                     override fun onStartTrackingTouch(self: SeekBar?) {
@@ -38,10 +79,12 @@ class AddLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
                     override fun onStopTrackingTouch(self: SeekBar?) {
                     }
-                })
+                }
+        )
     }
 
     override fun onMapReady(map: GoogleMap) {
+        this.googleMap = map;
         map.setOnMapClickListener {
             if(marker == null || circle == null) {
                 marker = map.addMarker(MarkerOptions()
@@ -49,7 +92,7 @@ class AddLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
                 circle = map.addCircle(CircleOptions()
                         .center(it)
-                        .radius(1000.0)
+                        .radius(radiusSeekBar.progress.toDouble())
                         .strokeWidth(5.0f)
                         .fillColor(Color.argb(0x55, 0x6A, 0xD9, 0xB3)))
             } else {
